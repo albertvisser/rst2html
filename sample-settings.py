0fb75eacc8e51ff3b8c97eb7269070e94ddef330
Albert Visser <visser@linuxmoby>
2010-07-28 21:13:26 +0200

    added sample settings and ignore file

---
+++ sample-settings.py
@@ -0,0 +1,16 @@
+# rename this file to settings.py to make it work
+
+# location of html pages
+root = "../target"
+# location of .rst sources
+source = "../source"
+# pad naar css
+css = "../target/style/html4css1.css"
+# location of (mirror) ste
+mirror = "../"
+# all css in separate files
+all_css = """\
+<link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
+<link rel="stylesheet" type="text/css" media="all" href="css/960.css" />
+<link rel="stylesheet" href="css/html4css1.css" type="text/css" />
+"""
