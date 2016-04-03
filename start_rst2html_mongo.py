ff8f544d3073a2d982c8440e6621f0133a41ab53
Albert Visser <albert@lemoncurry>
2016-04-03 11:23:08 +0200

    added startup stuff for rst2html_mongo

---
+++ start_rst2html_mongo.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import os
+## sys.stdout = sys.stderr
+import cgitb
+cgitb.enable()
+import cherrypy
+
+ROOT = os.path.dirname(os.path.abspath(__file__)) # '/home/albert/rst2html'
+os.chdir(ROOT)
+sys.path.insert(0, ROOT)
+from rst2html_mongo import Rst2Html
+
+
+
+application = cherrypy.tree.mount(Rst2Html())
+cherrypy.config.update({'environment': 'embedded'})
+cherrypy.config.update({'engine.autoreload_on': False,
+        })
