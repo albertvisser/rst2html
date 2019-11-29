678f8b403279bffb003eb8f99c5ea3b2ec2c37e6
Albert Visser <albert.visser@gmail.com>
2019-11-29 14:36:44 +0100

    #793 templates tabel toegevoegd aan database definitie

--- setup_database.sql
+++ setup_database.sql
@@ -10,28 +10,34 @@ CREATE TABLE sites (
 CREATE TABLE site_settings (
     id serial   PRIMARY KEY,
     site_id     INTEGER REFERENCES sites,
-    settname VARCHAR,
+    settname    VARCHAR,
     settval     VARCHAR
+    );
+CREATE TABLE templates (
+    id serial   PRIMARY KEY,
+    site_id     INTEGER REFERENCES sites,
+    name        VARCHAR,
+    text        VARCHAR
     );
 CREATE TABLE directories (
     id serial   PRIMARY KEY,
     site_id     INTEGER REFERENCES sites,
-    dirname  VARCHAR
+    dirname     VARCHAR
     );
 CREATE TABLE doc_stats (
-    id serial               PRIMARY KEY,
-    dir_id                  INTEGER REFERENCES directories,
-    docname            VARCHAR,
-    source_docid       INTEGER,   /* references TABLES[4] */
+    id serial       PRIMARY KEY,
+    dir_id          INTEGER REFERENCES directories,
+    docname         VARCHAR,
+    source_docid    INTEGER,   /* references TABLES[4] */
     source_updated  TIMESTAMP,
     source_deleted  BOOLEAN,
-    target_docid        INTEGER, /* references TABLES[4] */
-    target_updated   TIMESTAMP,
-    target_deleted   BOOLEAN,
-    mirror_updated   TIMESTAMP
+    target_docid    INTEGER,   /* references TABLES[4] */
+    target_updated  TIMESTAMP,
+    target_deleted  BOOLEAN,
+    mirror_updated  TIMESTAMP
     );
 CREATE TABLE documents (
-    id serial     PRIMARY KEY,
+    id serial   PRIMARY KEY,
     name        VARCHAR,
     previous    VARCHAR,
     currtext    VARCHAR
