07a6d45a3208aa0327b54c96a238511a3c23657e
Albert Visser <albert.visser@gmail.com>
2020-06-13 19:02:02 +0200

    #715/805 zoekfuncties toegevoegd

--- rst2html_functions.py
+++ rst2html_functions.py
@@ -882,6 +882,49 @@ def build_trefwoordenlijst(sitename, lang=LANG, sef=False):
     return "\n".join(data), has_errors
 
 
+def search_site(sitename, find, replace=None):
+    """return the results from the root and its subdirectories
+
+    walk the root to get results from it
+    then walk the subdirectories to add the results from there
+    """
+    results = read_dir(dml, sitename, find, replace)
+    for dirname in dml.list_dirs(sitename, 'src'):
+        results.update(read_dir(dml, sitename, find, replace, dirname))
+    return results
+
+
+def read_dir(dml, sitename, search, replace, dirname=''):
+    """return the results by walking a directory and getting results from the files in it
+
+    results in this case being search results (and replacements) in each document
+    """
+    results = collections.defaultdict(list)
+    for filename in dml.list_docs(sitename, 'src', directory=dirname):
+        results[(dirname, filename)] = process_file(dml, sitename, dirname, filename,
+                                                    search, replace)
+    return results
+
+
+def process_file(dml, sitename, dirname, filename, search, replace):
+    """do the finds and if needed the replacements
+    """
+    results = []
+    contents = dml.get_doc_contents(sitename, filename, 'src', dirname)
+    for number, line in enumerate(contents.split('\n')):
+        if search in line:
+            pos_list = []
+            pos = line.find(search)
+            while pos > -1:
+                pos_list.append(pos)
+                pos = line.find(search, pos + 1)
+            results.append((number + 1, line.strip(), [x + 1 for x in pos_list]))
+        if replace is not None:
+            new_contents = contents.replace(search, replace)
+            dml.update_rst(sitename, filename, new_contents, dirname)
+    return results
+
+
 class R2hState:
     """state class containing the functions called up from the web app
     """
